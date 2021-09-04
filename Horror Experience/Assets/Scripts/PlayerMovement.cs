142942f50fb51ae774b04ad917a39c4166f802de
sangmin <sm524233@naver.com>
2021-09-04 22:18:15 +0900

    Player #2(이동 / 회전)

---
+++ Horror Experience/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float turnSpeed = 20f;
+    Animator m_Animator;
+    Vector3 m_Movement;
+    Quaternion m_Rotation = Quaternion.identity;
+    Rigidbody m_Rigidbody;
+
+    void Start()
+    {
+        m_Animator = GetComponent<Animator>();
+        m_Rigidbody = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        m_Movement.Set(horizontal, 0f, vertical);
+        m_Movement.Normalize();
+
+        bool hasHorizontalInput = !Mathf.Approximately(horizontal, 0f); //수평이동이 있는지 여부 파악
+        bool hasVerticalInput = !Mathf.Approximately(vertical, 0f); //수직 ""
+        bool isWalking = hasHorizontalInput || hasVerticalInput; //수평, 수직 하나라도 있는지 확인
+        m_Animator.SetBool("IsWalking", isWalking);
+
+        Vector3 desiredForward = Vector3.RotateTowards(transform.forward, m_Movement, turnSpeed * Time.deltaTime, 0f);
+        m_Rotation = Quaternion.LookRotation(desiredForward);
+    }
+
+    void OnAnimatorMove() //루트 모션을 적용하여 이동과 회전을 개별적으로 적용
+    {
+        m_Rigidbody.MovePosition(m_Rigidbody.position + m_Movement * m_Animator.deltaPosition.magnitude);
+        m_Rigidbody.MoveRotation(m_Rotation);
+    }
+
+}
